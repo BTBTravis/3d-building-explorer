edee7475a7f02f56470daf1ff8bbedf1a3f73918
btbtravis <btbtravis@gmail.com>
2018-01-08 11:48:11 -0600

    clickable labels that pan camera

--- scripts/main.js
+++ scripts/main.js
@@ -51,16 +51,10 @@ requirejs(['THREE', 'ColladaLoader', 'Projector', 'SVGRenderer', 'OrbitControls'
       orbit.enableZoom = false;
       // TODO: Remove this dev code
       document.addEventListener('keydown', (e) => {
-        if (e.key === 'p') console.log({ orbit: orbit.getPolarAngle() });
-        else if (e.key === 'r') console.log({ modelRef: modelRef });
-        else if (e.key === '=') {
-          modelRef.outerShell.map((mesh) => {
-            mesh.material.opacity += 0.0005;
-          });
-        } else if (e.key === '-') {
-          modelRef.outerShell.map((mesh) => {
-            mesh.material.opacity -= 0.0005;
-          });
+        if (e.key === 't') {
+          console.log({cameraMatrix: camera.matrix});
+        } else if (e.key === 'p') {
+          console.log({cameraPos: JSON.stringify(camera.position)});
         }
       });
 
@@ -107,26 +101,102 @@ requirejs(['THREE', 'ColladaLoader', 'Projector', 'SVGRenderer', 'OrbitControls'
         modelRef.outerShell = outerShell;
         // move labels into place
         var locationCubes = meshes.filter((mesh) => { return mesh.material.name === 'location'; });
-
-        locationCubes.map(function (mesh) {
-          // mesh.position.set(1, -100, 1);
-        });
-        locationCubes[6].material = new THREE.MeshBasicMaterial({ color: 'green' });
+        // locationCubes[6].material = new THREE.MeshBasicMaterial({ color: 'green' });
         var labelPairs = {
-          'mlao': 0,
-          'rh': 1,
-          'pp': 2,
-          'ste': 3,
-          'ag': 4,
-          'lar': 5,
-          'rakt': 6,
-          'am': 7
+          'mlao': {
+            cubeIndex: 0,
+            cameraCords: {
+              x: 1085.423,
+              y: 1443.4489,
+              z: 2887.6134
+            }
+          },
+          'rh': {
+            cubeIndex: 1,
+            cameraCords: {
+              x: 394.681,
+              y: 546.976538,
+              z: 3338.419
+            }
+          },
+          'pp': {
+            cubeIndex: 2,
+            cameraCords: {
+              x: -935.137,
+              y: 1605.586,
+              z: 2854.4014
+            }
+          },
+          'ste': {
+            cubeIndex: 3,
+            cameraCords: {
+              x: 2656.39876,
+              y: 1605.58693,
+              z: 1402.011
+            }
+          },
+          'ag': {
+            cubeIndex: 4,
+            cameraCords: {
+              x: 2537.9710,
+              y: 1605.5869,
+              z: -1606.4848
+            }
+          },
+          'lar': {
+            cubeIndex: 5,
+            cameraCords: {
+              x: -2716.7743,
+              y: 1605.5869,
+              z: 1281.1040
+            }
+          },
+          'rakt': {
+            cubeIndex: 6,
+            cameraCords: {
+              x: 1776.091,
+              y: 1605.58693,
+              z: -2422.3108
+            }
+          },
+          'am': {
+            cubeIndex: 7,
+            cameraCords: {
+              x: 3287.484237,
+              y: 725.11662,
+              z: 516.384803
+            }
+          }
         };
         labels = labels.map(function (l) {
           var key = l.id.match(/label_(.+)/);
-          key = labelPairs[key[1]];
-          return [l, locationCubes[key]];
+          var labelPair = labelPairs[key[1]];
+          return {
+            elm: l,
+            cube: locationCubes[labelPair.cubeIndex],
+            cameraCords: labelPair.cameraCords
+          };
         });
+        // setup camera tweens to locations
+        labels.map(function (pair) {
+          pair.elm.addEventListener('click', function (e) {
+            var goalPos = pair.cameraCords;
+            var startPos = {
+              x: camera.position.x,
+              y: camera.position.y,
+              z: camera.position.z
+            };
+            var tween = TweenLite.to(startPos, 1, {x: goalPos.x, y: goalPos.y, z: goalPos.z, onUpdate: updateCameraPos});
+            // each time the tween updates this function will be called.
+            function updateCameraPos () {
+              console.log({startPos: startPos, goalPos: goalPos});
+              camera.position.set(startPos.x, startPos.y, startPos.z);
+              camera.lookAt(new THREE.Vector3(0, 0, 0));
+              camera.updateMatrix();
+            }
+          });
+        });
+
         // TODO: remove this dev code
         var mats = meshes.reduce((mats, mesh) => {
           if (!mats.includes(mesh.material.name)) mats.push(mesh.material.name);
@@ -141,7 +211,6 @@ requirejs(['THREE', 'ColladaLoader', 'Projector', 'SVGRenderer', 'OrbitControls'
         locationCubes.map(function (mesh) {
           console.log({ locationCube: mesh });
         });
-       
         resolve(); // fulfilled
       });
       // labels setup
@@ -179,17 +248,31 @@ requirejs(['THREE', 'ColladaLoader', 'Projector', 'SVGRenderer', 'OrbitControls'
     // pair = [label, wayfinding mesh]
     labels.map(function (pair) {
       // console.log(typeof pair);
-      if (typeof pair !== 'object') return pair;
       // translate 3d space to screen space
       var vector = new THREE.Vector3();
       var widthHalf = 0.5 * renderer.context.canvas.width;
       var heightHalf = 0.5 * renderer.context.canvas.height;
-      pair[1].updateMatrixWorld();
-      vector.setFromMatrixPosition(pair[1].matrixWorld);
+      pair.cube.updateMatrixWorld();
+      vector.setFromMatrixPosition(pair.cube.matrixWorld);
       vector.project(camera);
       vector.x = (vector.x * widthHalf) + widthHalf;
-      vector.y = - (vector.y * heightHalf) + heightHalf;
-      pair[0].setAttribute('transform', 'translate(' + vector.x + ', ' + (vector.y - pair[0].clientHeight) + ')');
+      vector.y = -(vector.y * heightHalf) + heightHalf;
+      // move label into position
+      pair.elm.setAttribute('transform', 'translate(' + vector.x + ', ' + (vector.y - pair.elm.clientHeight) + ')');
+      // find label distance from camera
+      var cubePos = pair.cube.getWorldPosition();
+      var cameraPos = camera.getWorldPosition();
+      var disFromCamera = cameraPos.sub(cubePos);
+      // fade labels by remaping the distance from camera to opacity on the svg
+      // TODO: switch to using three js math's .mapLinear https://threejs.org/docs/#api/math/Math
+      function remap (x, inMin, inMax, outMin, outMax) {
+        var remapVal = (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+        if (remapVal < outMin) remapVal = outMin;
+        else if (remapVal > outMax) remapVal = outMax;
+        return remapVal;
+      }
+      var opacity = remap(disFromCamera.length(), 2000, 5000, 0, 0.75);
+      pair.elm.style.opacity = 1 - opacity.toFixed(4);
     });
   }
 });
